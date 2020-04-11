c48811b9f510476c6be05e83cc7cb3d728573c77
SagredoIsmael <sagredo.ismael@live.com>
2020-04-11 21:26:04 +0200

    add dependencies

---
+++ index.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+
+
+const NavigationWrapper = ({App}) => {
+  <NavigationContainer>
+     <App/>
+  </NavigationContainer>
+}
+
+export default NavigationWrapper
