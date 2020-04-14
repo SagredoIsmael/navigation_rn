647eb53cd695bebbfc9f547c24c11787a4b67f10
SagredoIsmael <sagredo.ismael@live.com>
2020-04-14 11:05:42 +0200

    add navigator with one screen

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import 'react-native-gesture-handler'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import isArray from 'lodash/isArray'
+import map from 'lodash/map'
+import size from 'lodash/size'
+
+const Stack = createStackNavigator()
 
 
-const NavigationWrapper = ({App}) => {
-  <NavigationContainer>
-     <App/>
-  </NavigationContainer>
-}
+const NavigationWrapper = ({ screen, headerCommonsOptions }) =>
+  < NavigationContainer >
+    <Stack.Navigator initialRouteName={screen.name} screenOptions={headerCommonsOptions && headerCommonsOptions}>
+      <Stack.Screen name={screen.name} component={screen.component} options={screen.options} />
+    </Stack.Navigator>
+  </NavigationContainer >
+
 
 export default NavigationWrapper
