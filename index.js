eac454b3f37bf9432870227c0b45e803a0996b77
SagredoIsmael <sagredo.ismael@live.com>
2020-04-14 11:21:58 +0200

    add multiple stacks

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-
 import isArray from 'lodash/isArray'
 import map from 'lodash/map'
 import size from 'lodash/size'
 
 const Stack = createStackNavigator()
 
-
-const NavigationWrapper = ({ screen, headerCommonsOptions }) =>
+const NavigationWrapper = ({ screens, headerCommonsOptions }) =>
+  isArray(screens) && size(screens) > 0 &&
   < NavigationContainer >
-    <Stack.Navigator initialRouteName={screen.name} screenOptions={headerCommonsOptions && headerCommonsOptions}>
-      <Stack.Screen name={screen.name} component={screen.component} options={screen.options} />
+    <Stack.Navigator initialRouteName={screens[0].name} screenOptions={headerCommonsOptions && headerCommonsOptions}>
+      {map(screens, itemScreen =>
+        <Stack.Screen key={itemScreen.name} name={itemScreen.name} component={itemScreen.component} options={itemScreen.options} />
+      )}
     </Stack.Navigator>
   </NavigationContainer >
 
