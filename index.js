335d9b0f38596a06421da01a0dca042f3c85b890
SagredoIsmael <sagredo.ismael@live.com>
2020-04-20 00:15:35 +0200

    fix import && update v.1.0.15

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler'
-import React from 'react'
 import Navigation from './src/navigation'
 
-export default <Navigation/>
+export default Navigation
 
 export { default as connectNavigation } from './src/HOC'
